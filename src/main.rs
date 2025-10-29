0c394123e1898d791c1781d6421951d0217f3381
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2025-10-30 01:23:03 +0700

    Пацаны ваще ребята. (я потом коммит дропну)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+pub mod app;
+pub mod cache;
+pub mod rsync;
+
 fn main() {
     println!("bruh...");
 }
---
+++ src/rsync.rs
@@ -0,0 +1,10 @@
+//! Таска для синхронизации скинов DDNet
+//!
+//! Спавним и синхронизируем раз в день, юзаем CONDVAR
+//!
+//! Папка - .store
+//!
+//! Нужно фетчить только сам skin и community, только файлы с mime - png, либо просто спарси эту страницу апатча
+//!
+//! https://ddnet.org/skins/skin/
+//! https://ddnet.org/skins/skin/community/
